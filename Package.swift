f9542c384efbd12ec5b2e75d5295dd6142b70c2f
Christian Wolf Johannsen <chwo@mapspeople.com>
2024-08-26 15:26:01 +0200

    Release 4.5.12

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let mapsindoorsVersion = Version("4.5.12")
+
+let package = Package(
+    name: "MapsIndoorsGoogleMaps",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "MapsIndoorsGoogleMaps",
+            targets: ["MapsIndoorsGoogleMaps"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/MapsPeople/mapsindoors-core-ios.git", exact: mapsindoorsVersion),
+        .package(url: "https://github.com/googlemaps/ios-maps-sdk.git", exact: "8.4.0"),
+        .package(url: "https://github.com/brownsoo/ValueAnimator.git", from: "0.6.9")
+    ],
+    targets: [
+        .target(
+            name: "MapsIndoorsGoogleMaps",
+            dependencies: [
+                .product(name: "MapsIndoorsCore", package: "mapsindoors-core-ios"),
+                .product(name: "GoogleMaps", package: "ios-maps-sdk"),
+                .product(name: "GoogleMapsBase", package: "ios-maps-sdk"),
+                .product(name: "GoogleMapsCore", package: "ios-maps-sdk"),
+                .product(name: "ValueAnimator", package: "ValueAnimator")
+            ]
+        ),
+    ]
+)
